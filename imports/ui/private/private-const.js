41ec7bb4c4ec666931c7290db3bc8220ba7ec240
ekimretrop <ekim.retrop@gmail.com>
2017-05-04 19:51:25 -0400

    changes from Lilith after first review of app running on david's server

--- imports/ui/private/private-const.js
+++ imports/ui/private/private-const.js
@@ -1,6 +1,16 @@
-RECAPTCHA_SECRET_KEY="SetAtBuildTime";
-RECAPTCHA_SITE_KEY="SetAtBuildTime";
-CTOP_SEND_REAL_EMAILS="SetAtBuildTime";
-CTOP_EMAIL_ACCOUNT="SetAtBuildTime";
-CTOP_EMAIL_PASSWORD="SetAtBuildTime";
-CTOP_REDIRECT_EMAIL_FOR_TESTING = "SetAtBuildTime";
+RECAPTCHA_SECRET_KEY="6LdnlBMUAAAAAMScf0hlWfTKHPZIBxhYQxQ3gG1A";
+RECAPTCHA_SITE_KEY="6LdnlBMUAAAAAH9f3S__UqixQV-w-0D0MdMRlwqY";
+CTOP_SEND_REAL_EMAILS=true;
+CTOP_EMAIL_ACCOUNT="carrot.lovingspoonful.org@gmail.com";
+CTOP_EMAIL_PASSWORD="LionzTigrzBairz17";
+CTOP_REDIRECT_EMAIL_FOR_TESTING = "mike__porter@hotmail.com";
+
+
+//siteKey:
+// "6LcS2RgUAAAAMUYmbmBGepCxj9SyFb2RMVSMK-1",  // prod - carrottop url
+// "6LdnlBMUAAAAAH9f3S__UqixQV-w-0D0MdMRlwqY", // local host
+
+// secret key
+// "6LdnlBMUAAAAAMScf0hlWfTKHPZIBxhYQxQ3gG1A" // localhost
+// "6LcS2RgUAAAACgpDwRJsp-ajYaS2W_olXPQm1j3" // for prod
+// LilithWyattCTopForDL
