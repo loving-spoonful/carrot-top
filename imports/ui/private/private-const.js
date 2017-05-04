1fa1944fff6db2035fc2336ff383474aa218787f
ekimretrop <ekim.retrop@gmail.com>
2017-05-04 19:51:25 -0400

    changes from Lilith after first review of app running on david's server

--- imports/ui/private/private-const.js
+++ imports/ui/private/private-const.js
@@ -3,4 +3,14 @@ RECAPTCHA_SITE_KEY="SetAtBuildTime";
 CTOP_SEND_REAL_EMAILS="SetAtBuildTime";
 CTOP_EMAIL_ACCOUNT="SetAtBuildTime";
 CTOP_EMAIL_PASSWORD="SetAtBuildTime";
-CTOP_REDIRECT_EMAIL_FOR_TESTING = "SetAtBuildTime";
+CTOP_REDIRECT_EMAIL_FOR_TESTING="SetAtBuildTime";
+
+
+//siteKey:
+// prod - carrottop url
+// local host
+
+// secret key
+// localhost
+// for prod
+// LilithWyattCTopForDL
