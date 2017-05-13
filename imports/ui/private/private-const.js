f71e32cd6a4c69c13c7fef81b593015006edb605
David Lougheed <david.lougheed@gmail.com>
2017-05-13 18:35:16 -0400

    Work on cleaning up private const file.

--- imports/ui/private/private-const.js
+++ imports/ui/private/private-const.js
@@ -4,13 +4,3 @@ CTOP_SEND_REAL_EMAILS="SetAtBuildTime";
 CTOP_EMAIL_ACCOUNT="SetAtBuildTime";
 CTOP_EMAIL_PASSWORD="SetAtBuildTime";
 CTOP_REDIRECT_EMAIL_FOR_TESTING="SetAtBuildTime";
-
-
-//siteKey:
-// prod - carrottop url
-// local host
-
-// secret key
-// localhost
-// for prod
-// LilithWyattCTopForDL
