3280c78b248f42401b8a449f6a2f933823f03fc5
David Lougheed <david.lougheed@gmail.com>
2017-05-14 01:25:24 -0400

    Add untested Windows deployment script.

---
+++ deploy_on_server.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Decompress application bundle
+tar -zxf ~/loving-spoonful.tar.gz
+
+# Install Dependencies
+cd ~/bundle/programs/server
+sudo npm install > /dev/null
+
+# Fix Permissions
+sudo chown -R lsapp:lsapp /home/lsapp
+
+# Restart Service
+sudo restart lsapp
