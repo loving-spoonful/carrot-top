84732c3936335dfd28880740b7ba91602d9f66ac
David Lougheed <david.lougheed@gmail.com>
2017-06-25 20:56:55 -0400

    Fix bad information being committed.

--- imports/ui/private/private-const.js
+++ imports/ui/private/private-const.js
@@ -3,15 +3,4 @@ RECAPTCHA_SITE_KEY="SetAtBuildTime";
 CTOP_SEND_REAL_EMAILS="SetAtBuildTime";
 CTOP_EMAIL_ACCOUNT="SetAtBuildTime";
 CTOP_EMAIL_PASSWORD="SetAtBuildTime";
-CTOP_REDIRECT_EMAIL_FOR_TESTING="SetAtBuildTime";
-
-
-//siteKey:
-// prod - carrottop url
-// local host
-
-// secret key
-// localhost
-// for prod
-// LilithWyattCTopForDL
-
+CTOP_REDIRECT_EMAIL_FOR_TESTING = "SetAtBuildTime";
