83362628f1c3452049012c620b674f80c8e85229
David Lougheed <david.lougheed@gmail.com>
2017-09-24 23:45:06 -0400

    Fix windows deploy code (still untested).

--- deploy_to_server.bat
+++ deploy_to_server.bat
@@ -2,8 +2,15 @@ echo This will create a loving-spoonful-build folder in the parent directory of 
 
 pause
 
+echo
+echo --- Building ---
+
 rmdir ..\loving-spoonful-build /s
 mkdir ..\loving-spoonful-build
 meteor build ..\loving-spoonful-build --server-only --server carrot.lovingspoonful.org
-pscp -scp lsapp@66.228.44.6:~
-plink lsapp@MachineB -m deploy_on_server.sh
+
+echo --- Copying to Server ---
+pscp -scp lsapp@66.228.44.6:~ ..\loving-spoonful-build\loving-spoonful.tar.gz
+
+echo --- Connecting to Server ---
+plink lsapp@66.228.44.6 -m deploy_on_server.sh
